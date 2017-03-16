c09b4e1fe8e6bd64894dd2a9e3c6933bcec8d16c
leichaofan <leichaofan2024@gmail.com>
2017-03-16 17:48:35 +0800

    generate controller admin jobs/resumes and complete the CURD of jobs

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,45 @@
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :require_is_admin
+  def index
+    @jobs = Job.all
+  end
+  def show
+    @job = Job.find(params[:id])
+  end
+  def new
+    @job = Job.new
+  end
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to admin_jobs_path, notice: "创建成功！"
+    else
+      render :new
+    end
+  end
+  def edit
+    @job = Job.find(params[:id])
+  end
+  def update
+    @Job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path, notice: "更新成功！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path, warning: "已成功删除！"
+  end
+
+  private
+
+  def job_params
+     params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :jobs
+    resources :resumes
+  end 
+
   root "jobs#index"
   resources :jobs do
     resources :resumes
-  end 
+  end
 end
