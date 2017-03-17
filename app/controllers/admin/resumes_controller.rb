f44adbe93c189783c2995f8001e096c4a98aa95d
leichaofan <leichaofan2024@gmail.com>
2017-03-17 14:27:39 +0800

    solve the 'have no method of email' bug

--- app/controllers/admin/resumes_controller.rb
+++ app/controllers/admin/resumes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ResumesController < ApplicationController
-
+  layout "admin"
   def index
     @job = Job.find(params[:job_id])
     @resumes = @job.resumes
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -13,33 +13,33 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
-  def new
-    @job = Job.new
-  end
-  def create
-    @job = Job.new(job_params)
-    if @job.save
-      redirect_to admin_jobs_path, notice: "创建成功！"
-    else
-      render :new
-    end
-  end
-  def edit
-    @job = Job.find(params[:id])
-  end
-  def update
-    @Job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to admin_jobs_path, notice: "更新成功！"
-    else
-      render :edit
-    end
-  end
-  def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-    redirect_to admin_jobs_path, notice: "已成功删除！"
-  end
+  # def new
+  #   @job = Job.new
+  # end
+  # def create
+  #   @job = Job.new(job_params)
+  #   if @job.save
+  #     redirect_to admin_jobs_path, notice: "创建成功！"
+  #   else
+  #     render :new
+  #   end
+  # end
+  # def edit
+  #   @job = Job.find(params[:id])
+  # end
+  # def update
+  #   @Job = Job.find(params[:id])
+  #   if @job.update(job_params)
+  #     redirect_to admin_jobs_path, notice: "更新成功！"
+  #   else
+  #     render :edit
+  #   end
+  # end
+  # def destroy
+  #   @job = Job.find(params[:id])
+  #   @job.destroy
+  #   redirect_to admin_jobs_path, notice: "已成功删除！"
+  # end
 
   private
 
